74b2f7ce17a6b95f2a4ea2a923e205ce8e42587d
OsmanBytyqi <75097365+OsmanBytyqi@users.noreply.github.com>
2021-05-18 00:34:43 +0200

    decode button

--- ConsoleApp/Steganografi/Program.cs
+++ ConsoleApp/Steganografi/Program.cs
@@ -129,6 +129,35 @@ namespace Steganografi
         private void button2_Click(object sender, EventArgs e)
         {
            //Butoni per dekodim
+           Bitmap img = new Bitmap(textBox2.Text);
+            string message = "";
+
+            Color lastpixel = img.GetPixel(img.Width - 1, img.Height - 1);
+            int msgLength = lastpixel.B;
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if (i < 1 && j < msgLength)
+                    {
+                        int value = pixel.B;
+                        char c = Convert.ToChar(value);
+                        string letter = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(c) });
+
+                        message = message + letter;
+                    }
+                }
+            }
+
+            textBox1.Text = message;
+           
+           
+           
+           
+           
            
         }
 
