e3ccda8d3684060a26f33dd63fe9e424ddd42128
Valdrin-Ejupi <75037161+Valdrin-Ejupi@users.noreply.github.com>
2021-07-23 19:19:32 +0200

    Add files via upload

---
+++ Projekti2/Server TCP/Server_TCP/Shfrytezuesi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_TCP
+{
+    public class Shfrytezuesi
+    {
+        public string userId { get; set; }
+        public string PasswordHash { get; set; }
+        public string Fatura { get; set; }
+        public decimal Vlera { get; set; }
+        public string Muaji { get; set; }
+        public int Viti { get; set; }
+        public string Email { get;set;}
+    }
+}
