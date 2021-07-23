af5bb47218a77114cea1b363e29e65eab117d033
Valdrin-Ejupi <75037161+Valdrin-Ejupi@users.noreply.github.com>
2021-07-23 18:59:57 +0200

    Add files via upload

---
+++ Projekti2/Server TCP/Server_TCP/Jwt.cs
@@ -0,0 +1,41 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_TCP
+{
+    class Jwt
+    {
+        Shfrytezuesi m = new Shfrytezuesi();
+        private static RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+        private static RSAParameters privateKey = rsa.ExportParameters(true);
+
+        public String createToken(String userId, int Viti, String Fatura, Decimal Vlera, String Muaji,String Email)
+        {
+            var payload = new Dictionary<string, object>
+        {
+            { "userId", userId },
+            { "Viti", Viti },
+            {"Fatura", Fatura },
+            {"Vlera", Vlera },
+            {"Muaji", Muaji },
+            {"Email",Email}
+        };
+
+
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+            IJsonSerializer serializer = new JsonNetSerializer(); 
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+            var token = encoder.Encode(payload, privateKey.ToString());
+            return token;
+        }
+        
+    }
+}
